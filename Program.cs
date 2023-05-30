549e7cbb1998befb71985dd76350d308e1f2ab62
ROMA <igoshin1922@gmail.com>
2023-05-31 02:43:41 +0500

    ff

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 > number2) {
+//     Console.WriteLine($"Число {number1} больше чем {number2}");
+// } else if(number1 == number2)
+// {
+//     Console.WriteLine($"Число {number1} равно {number2}");
+// } else 
+// {
+//     Console.WriteLine($"Число {number2} больше чем {number1}");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//  Console.WriteLine("Введите первое число");
+//  int number1 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите второе число");
+//  int number2 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите третье число");
+//  int number3 = Convert.ToInt32(Console.ReadLine());
+
+//  int max = number1;
+
+// if (max< number2) {
+//     max = number2;
+// }
+//  if(max < number3) {
+//     max = number3;
+//  }
+//   Console.WriteLine(max);
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//  Console.WriteLine("Введите  число");
+//  int number = Convert.ToInt32(Console.ReadLine());
+
+//  if (number % 2 == 0) {
+//      Console.WriteLine("Число чётное!");
+//  } else 
+//  {
+//      Console.WriteLine("Число не чётное!");
+//  }
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите  число  ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = 1; i < number + 1; i++) {
+//     if(i % 2 == 0) {
+//         Console.WriteLine(i);
+//     }
+// }
